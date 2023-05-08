566c09e05780b173a186999f25ca4c2f80aa2e66
NachoMol <ignaciotmolina@gmail.com>
2023-05-08 20:27:32 -0300

    creando interfaz Grupo

---
+++ Clase1S_ejercicio1/src/presencial/Grupo.java
@@ -0,0 +1,19 @@
+package presencial;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grupo {
+
+    //Coleccion
+    private List<Persona> grupo;
+
+    public Grupo(){
+        grupo = new ArrayList<>();
+    }
+
+    public void newPersona(Persona persona){
+        grupo.add(persona);
+    }
+}
