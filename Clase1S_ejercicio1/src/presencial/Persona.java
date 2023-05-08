1f78a6357df204799d79a21938b5d92aa1377e94
NachoMol <ignaciotmolina@gmail.com>
2023-05-08 19:58:43 -0300

    inicializando proyecto clase 1

---
+++ Clase1S_ejercicio1/src/presencial/Persona.java
@@ -0,0 +1,14 @@
+package presencial;
+
+public class Persona {
+    private Integer edad;
+    private String nombre;
+
+    public Persona(Integer edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    
+
+}
