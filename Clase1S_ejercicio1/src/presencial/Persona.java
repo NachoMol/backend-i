dd1685d3bfdb0c81b8d1bba4723bf156931cec46
NachoMol <ignaciotmolina@gmail.com>
2023-05-08 20:13:53 -0300

    Subiendo constructor, getters y setters para persona

--- Clase1S_ejercicio1/src/presencial/Persona.java
+++ Clase1S_ejercicio1/src/presencial/Persona.java
@@ -9,6 +9,38 @@ public class Persona {
         this.nombre = nombre;
     }
 
-    
+    public Integer getEdad() {
+        return edad;
+    }
 
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean checkEdad(){
+        return edad >= 18;
+    }
+
+    public boolean checkEdadPosible (){
+        //definimos una respuesta estandar o inicial
+        //operar lo que realice el método
+        //devolver la respuesta
+
+        boolean respuesta= false;
+
+        if (edad>= 0 && edad<=120){
+            respuesta = true;
+        }
+
+        return respuesta;
+
+    }
 }
