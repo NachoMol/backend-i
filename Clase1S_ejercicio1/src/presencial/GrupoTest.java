2f05ec28572e95d7d5481e1797ee91f29c4500e8
NachoMol <ignaciotmolina@gmail.com>
2023-05-08 22:08:30 -0300

    subiendo caso de prueba adicional

--- Clase1S_ejercicio1/src/presencial/GrupoTest.java
+++ Clase1S_ejercicio1/src/presencial/GrupoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GrupoTest {
     @Test
-    public void agregar5personasOkTest() {
+    public void agregar5personasNombresCortosOkTest() {
         //Dado
         Persona juan = new Persona(25,"Juan");
         Persona pedro = new Persona(25,"Pedro");
@@ -22,6 +22,27 @@ class GrupoTest {
         grupo1.newPersona(julian);
         //Entonces
         assertTrue(grupo1.cantidadDePersonas() == 2);
+    }
+
+    @Test
+    public void agregarPersonaMenoresOkTest() {
+        //Dado
+        Persona juan = new Persona(12,"Juan");
+        Persona pedro = new Persona(12,"Pedro");
+        Persona ana = new Persona(23,"Ana");
+        Persona luz = new Persona(14,"Luz");
+        Persona julian = new Persona(24,"Julian");
+        Grupo grupo2 = new Grupo();
+        //Cuando
+        grupo2.newPersona(juan);
+        grupo2.newPersona(pedro);
+        grupo2.newPersona(ana);
+        grupo2.newPersona(luz);
+        grupo2.newPersona(julian);
+        //Entonces
+        System.out.println("Cantidad de personas:"+grupo2.cantidadDePersonas());
+        assertTrue(grupo2.cantidadDePersonas() == 1);
+        System.out.println(grupo2);
     }
 
 }
