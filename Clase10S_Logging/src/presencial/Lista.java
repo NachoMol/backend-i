706c80ab2c5008ec26f7a9f304d18f5a7b55ec16
NachoMol <ignaciotmolina@gmail.com>
2023-05-17 21:39:53 -0300

    subiendo loggers

---
+++ Clase10S_Logging/src/presencial/Lista.java
@@ -0,0 +1,13 @@
+package presencial;
+
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class Lista {
+
+    private static final Logger logger= Logger.getLogger(Lista.class);
+
+    private List<Integer> listaEnteros;
+
+}
