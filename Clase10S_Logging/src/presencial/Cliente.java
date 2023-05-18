042c095c6fc24c24bfc178e83accf5593b35ece6
NachoMol <ignaciotmolina@gmail.com>
2023-05-17 21:47:17 -0300

    inicializando main con primer prueba

---
+++ Clase10S_Logging/src/presencial/Cliente.java
@@ -0,0 +1,17 @@
+package presencial;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+
+        Lista listadoEnteros = new Lista();
+
+        for(int i = 0; i < 6; i++){
+            listadoEnteros.agregarEnteros(10);
+        }
+
+        
+
+    }
+
+}
--- Clase10S_Logging/src/presencial/Lista.java
+++ Clase10S_Logging/src/presencial/Lista.java
@@ -2,6 +2,7 @@ package presencial;
 
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lista {
@@ -9,5 +10,20 @@ public class Lista {
     private static final Logger logger= Logger.getLogger(Lista.class);
 
     private List<Integer> listaEnteros;
+
+    public Lista() {
+        listaEnteros = new ArrayList<>();
+    }
+
+    public void agregarEnteros(Integer numero){
+        listaEnteros.add(numero);
+
+        if(listaEnteros.size()>5){
+            logger.info("La lista creada tiene: " + listaEnteros.size() + " numeros enteros, superando los 5 elementos");
+        }
+
+    }
+
+
 
 }
