111963ff423af3e968e16897b2034e01480afe6d
NachoMol <ignaciotmolina@gmail.com>
2023-05-24 20:29:18 -0300

    implementando PacienteService

---
+++ Clase15S_integradora/src/service/PacienteService.java
@@ -0,0 +1,36 @@
+package service;
+
+import dao.IDao;
+import model.Paciente;
+
+import java.util.List;
+
+public class PacienteService {
+
+    private IDao<Paciente> pacienteIDao;
+
+    public PacienteService(IDao<Paciente> pacienteIDao) {
+        this.pacienteIDao = pacienteIDao;
+    }
+
+    public Paciente altaPaciente(Paciente p){
+        return pacienteIDao.guardar(p);
+    }
+
+    public Paciente buscarPaciente(int id){
+        return pacienteIDao.buscar(id);
+    }
+
+    public Paciente modificarPaciente(Paciente p){
+        return pacienteIDao.modificar(p);
+    }
+
+    public void eliminarPaciente (int id){
+        pacienteIDao.eliminar(id);
+    }
+
+    public List<Paciente> listarPaciente(){
+        return pacienteIDao.listar():
+    }
+
+}
