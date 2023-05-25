0914e0a495251565bd41b1d6ee2d5051f3fbdc73
NachoMol <ignaciotmolina@gmail.com>
2023-05-25 18:25:54 -0300

    Terminando clase integradora pre-parcial

--- Clase15S_integradora/src/dao/PacienteDAOH2.java
+++ Clase15S_integradora/src/dao/PacienteDAOH2.java
@@ -6,10 +6,7 @@ import model.Paciente;
 import org.apache.log4j.Logger;
 import service.PacienteService;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +14,49 @@ public class PacienteDAOH2 implements IDao<Paciente> {
     private static final String SQL_SELECT_ALL="SELECT * FROM PACIENTES";
     private static final String SQL_SELECT_ONE="SELECT * FROM PACIENTES WHERE ID=?";
 
+    private static final String SQL_MODIFICAR="UPDATE PACIENTES SET APELLIDO=?, NOMBRE=?, DNI=?, FECHA=? WHERE ID = ?";
+
+    private static final String SQL_INSERT_PACIENTE = "INSERT INTO PACIENTES VALUES(?,?,?,?,?,?)";
+
+    private static final String SQL_ELIMINAR="DELETE FROM PACIENTES WHERE ID = ?";
+
+    private static final Logger LOGGER= Logger.getLogger(PacienteDAOH2.class);
 
     @Override
     public Paciente guardar(Paciente paciente) {
 
+        LOGGER.debug("ingresamos al metodo guardar de paciente en PacienteDaoH2");
+        Connection connection= null;
+        Domicilio domicilio = null;
+        DomicilioDAOH2 domicilioAux = new DomicilioDAOH2();
+        domicilio=domicilioAux.buscar(paciente.getDomicilio().getId());
+
+        try {
+            //ID, APELLIDO,NOMBRE,DNI(S),FECHA INGRESO,DOMICILIO
+            connection = BD.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT_PACIENTE);
+
+            preparedStatement.setInt(1,paciente.getId());
+            preparedStatement.setString(2,paciente.getApellido());
+            preparedStatement.setString(3, paciente.getNombre());
+            preparedStatement.setString(4, paciente.getDocumento());
+            //??? preguntar!!!!!!!!
+            preparedStatement.setDate(5, Date.valueOf(paciente.getFechaIngreso()));
+            preparedStatement.setInt(6, domicilio.getId());
+            // ejecutamos
+            preparedStatement.execute();
+
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
         return paciente;
     }
 
@@ -57,12 +93,49 @@ public class PacienteDAOH2 implements IDao<Paciente> {
 
     @Override
     public void modificar(Paciente paciente) {
+        LOGGER.info("ingresamos al metodo modificar el id: " + paciente.getId());
+        Connection connection=null;
 
+        try{
+            connection = BD.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_MODIFICAR);
+            //APELLIDO,NOMBRE,DNI(S),FECHA INGRESO
+            preparedStatement.setString(1, paciente.getApellido());
+            preparedStatement.setString(2, paciente.getNombre());
+            preparedStatement.setDate(3, Date.valueOf(paciente.getFechaIngreso()));
+            preparedStatement.setInt(4,paciente.getId());
+            preparedStatement.execute();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void eliminar(int id) {
+        LOGGER.debug("INGRESAMOS AL METODO DE eliminar ID POR paciente : " + id);
+        Connection connection=null;
+        try{
+            connection = BD.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_ELIMINAR);
+            preparedStatement.setInt(1,id);
+            preparedStatement.execute();
 
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Override
