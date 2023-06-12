e2d5347b472e9c2a3a2f146bde3dbf00a8096664
NachoMol <ignaciotmolina@gmail.com>
2023-06-12 20:38:14 -0300

    agregando @Repository

--- clase31/src/main/java/com/example/clase31/repository/MovimientoRepository.java
+++ clase31/src/main/java/com/example/clase31/repository/MovimientoRepository.java
@@ -2,6 +2,8 @@ package com.example.clase31.repository;
 
 import com.example.clase31.entity.Movimiento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovimientoRepository extends JpaRepository<Movimiento,Long> {
 }
