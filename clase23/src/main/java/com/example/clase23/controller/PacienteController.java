31b732a44ab4b1ea0efb0a94aa8977f5ffc18a97
NachoMol <ignaciotmolina@gmail.com>
2023-06-24 14:14:17 -0300

    subiendo loggers PacienteController y TurnoController

--- clase23/src/main/java/com/example/clase23/controller/PacienteController.java
+++ clase23/src/main/java/com/example/clase23/controller/PacienteController.java
@@ -9,25 +9,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.apache.log4j.Logger;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+
+    private static final Logger logger= Logger.getLogger(PacienteController.class);
    @Autowired
     private PacienteService pacienteService;
 
     @PostMapping
     public ResponseEntity<Paciente> agregarPaciente(@RequestBody Paciente paciente){
+        logger.info("Procesando solicitud de agregar paciente");
         return ResponseEntity.ok(pacienteService.guardarPaciente(paciente));
     }
     @GetMapping("/id/{id}")
     public ResponseEntity<Paciente> buscarPaciente(@PathVariable Long id) throws BadRequestException{
+        logger.info("Procesando solicitud de buscar paciente");
         Optional<Paciente> pacienteBuscado= pacienteService.buscarPacientePorID(id);
         if (pacienteBuscado.isPresent()){
             return ResponseEntity.ok(pacienteBuscado.get());
         }else{
+            logger.error("Falló la solicitud de buscar paciente");
             throw new BadRequestException("Paciente no encontrado");
         }
 
@@ -36,21 +42,25 @@ public class PacienteController {
 
     @GetMapping("/{correo}")
     public ResponseEntity<Paciente> buscarPacientePorCorreo(@PathVariable String correo) throws ResourceNotFoundException{
+        logger.info("Procesando solicitud de buscar paciente por correo");
         Optional<Paciente> pacienteBuscado= pacienteService.buscarPacientePorCorreo(correo);
         if(pacienteBuscado.isPresent()){
             return ResponseEntity.ok(pacienteBuscado.get());
         }else{
+            logger.error("Falló la solicitud de buscar paciente por correo");
             throw new ResourceNotFoundException("No existe el correo asociado en la base de datos.");
         }
     }
 
     @GetMapping
     public ResponseEntity<List<Paciente>> listarPacientes(){
+        logger.info("Procesando solicitud de listar pacientes");
         return ResponseEntity.ok(pacienteService.listarPacientes());
     }
 
     @PutMapping
     public ResponseEntity<String> modificarPaciente(@RequestBody Paciente paciente) throws ResourceNotFoundException{
+        logger.info("Procesando solicitud de modificar paciente");
         //vamos a consultar si ese paciente existe
         Optional<Paciente> pacientebuscado= pacienteService.buscarPacientePorID(paciente.getId());
         if(pacientebuscado.isPresent()){
@@ -58,6 +68,7 @@ public class PacienteController {
             return ResponseEntity.ok("Paciente Actualizado -"+paciente.getNombre()+" "+paciente.getApellido());
         }
         else{
+            logger.error("Falló la solicitud de modificar paciente");
             throw new ResourceNotFoundException("Paciente No Encontrado: "+ "Id: " + paciente.getId()+ "Nombre: "+paciente.getNombre());
         }
 
@@ -65,12 +76,14 @@ public class PacienteController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> eliminarPaciente(@PathVariable long id) throws ResourceNotFoundException {
+        logger.info("Procesando solicitud de eliminar paciente");
         Optional<Paciente> pacienteBuscado = pacienteService.buscarPacientePorID(id);
 
         if(pacienteBuscado.isPresent()){
             pacienteService.eliminarPaciente(id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Se elimino correctamente el paciente");
         }else{
+            logger.error("Falló la solicitud de eliminar paciente");
             throw new ResourceNotFoundException("No existe el id asociado en la base de datos" + id);
         }
     }
--- clase23/src/main/java/com/example/clase23/controller/TurnoController.java
+++ clase23/src/main/java/com/example/clase23/controller/TurnoController.java
@@ -12,7 +12,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.apache.log4j.Logger;
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +20,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/turnos")
 public class TurnoController {
+
+    private static final Logger logger= Logger.getLogger(TurnoController.class);
     private TurnoService turnoService;
     private PacienteService pacienteService;
     private OdontologoService odontologoService;
@@ -31,11 +33,13 @@ public class TurnoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Turno>> traerTurnos(){
+    public ResponseEntity<List<Turno>> listarTurnos(){
+        logger.info("Procesando solicitud de listar turnos");
         return ResponseEntity.ok(turnoService.devolverTurnos());
     }
     @PostMapping
     public ResponseEntity<Turno> registrarTurno(@RequestBody Turno turno) throws BadRequestException{
+        logger.info("Procesando solicitud de registrar un turno");
         //tratamiento
         Optional<Paciente> pacienteBuscado=pacienteService.buscarPacientePorID(turno.getId());
         Optional<Odontologo> odontologoBuscado=odontologoService.buscarOdontologoPorId(turno.getOdontologo().getId());
@@ -43,17 +47,19 @@ public class TurnoController {
             return ResponseEntity.ok(turnoService.guardarTurno(turno));
         }
         else{
-            //solo devolvemos el código bad request
+            logger.error("Falló la solicitud de registrar un turno");
             throw new BadRequestException("No existe el id asociado en la base de datos");
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> eliminarTurno(@PathVariable Long id) throws ResourceNotFoundException{
+        logger.info("Procesando solicitud de eliminar un turno");
         Optional<Turno> turnoBuscado= turnoService.buscarTurnoPorId(id);
         if(turnoBuscado.isPresent()){
             return ResponseEntity.ok("Se elimino el turno corrrectamente con id: " + id);
         }else{
+            logger.error("Falló la solicitud de eliminar un turno");
             throw new ResourceNotFoundException("No existe el id asociado en la base de datos" + id);
         }
     }
