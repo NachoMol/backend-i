4491e36f2a5b98e5c3d21b5abad804ef3af0cbc7
NachoMol <ignaciotmolina@gmail.com>
2023-06-26 19:33:08 -0300

    Implementando entities de Spring Security

---
+++ clase23/src/main/java/com/example/clase23/entities/AppUsuario.java
@@ -0,0 +1,76 @@
+package com.example.clase23.entities;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+
+
+@Getter
+@Setter
+@Entity
+public class AppUsuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private AppUsuarioRole appUsuarioRole;
+
+    public AppUsuario() {
+    }
+
+    public AppUsuario(String nombre, String apellido, String email, String password, AppUsuarioRole appUsuarioRole) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.appUsuarioRole = appUsuarioRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ clase23/src/main/java/com/example/clase23/entities/AppUsuarioRole.java
@@ -0,0 +1,5 @@
+package com.example.clase23.entities;
+
+public enum AppUsuarioRole {
+    ROLE_USER,ROLE_ADMIN
+}
